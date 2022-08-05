99510ede580337b2a53022a2ea03b9c39cd8d939
Dmytro <103268527+DiAn445@users.noreply.github.com>
2022-08-06 00:02:46 +0300

    add start

--- DickSizeBot.py
+++ DickSizeBot.py
@@ -1,5 +1,6 @@
 import random
-from time import ctime
+import time
+from time import ctime, sleep
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.types import message
@@ -9,40 +10,53 @@ import os, hashlib
 import logging
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-
-
 logging.basicConfig(level=logging.INFO)
 
 storage = MemoryStorage()
-bot = Bot(token='', parse_mode=types.ParseMode.HTML)
+bot = Bot(token='5172128174:AAEqCX6SfE-HHERZ7wH0Emls5oAvqTepQts', parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=storage)
 
 
-data = {}
+class DB:
+    data = {}
+    counter_list = []
 
 @dp.inline_handler()
 async def inline_handler(query: types.InlineQuery):
+    async def data_cleaner():
+        """to clean data at the next day"""
+        while True:
+            if len(DB.counter_list) < 7 and str(ctime().split()[0]) not in DB.counter_list:
+                DB.counter_list.append(str(ctime().split()[0]))
+                DB.data.clear()
+                return
+            elif len(DB.counter_list) == 7:
+                DB.counter_list.clear()
+                continue
+            return
+
+    await data_cleaner()
     name = query.from_user.id
     text = query.query
     ran = randint(1, 40)
-    if f'{name}' in data:
-        link = data[f'{name}']
+    if f'{name}' in DB.data:
+        link = DB.data[f'{name}']
     else:
         if ran <= 5:
             link = f'<i>Your dick size for today is:</i> <b>{ran}cm</b> 🤡'
-            data[f'{name}'] = link
+            DB.data[f'{name}'] = link
         elif 5 < ran <= 15:
             link = f'<i>Your dick size for today is:</i> <b>{ran}cm</b> 🤫'
-            data[f'{name}'] = link
+            DB.data[f'{name}'] = link
         elif 15 < ran <= 25:
             link = f'<i>Your dick size for today is:</i> <b>{ran}cm</b> 💪'
-            data[f'{name}'] = link
+            DB.data[f'{name}'] = link
         elif 25 < ran <= 35:
             link = f'<i>Your dick size for today is:</i> <b>{ran}cm</b> 😎'
-            data[f'{name}'] = link
+            DB.data[f'{name}'] = link
         elif 35 < ran <= 40:
             link = f'<i>Your dick size for today is:</i> <b>{ran}cm</b> 😍'
-            data[f'{name}'] = link
+            DB.data[f'{name}'] = link
     result_id: random.randint(1, 5) = hashlib.md5(text.encode()).hexdigest()
 
     articles = [types.InlineQueryResultArticle(
@@ -50,8 +64,7 @@ async def inline_handler(query: types.InlineQuery):
         title='🎲 Dick size',
         input_message_content=types.InputTextMessageContent(
             message_text=link, parse_mode=types.ParseMode.HTML))]
+
     await query.answer(articles, cache_time=1, is_personal=True)
-
-
 
 executor.start_polling(dp, skip_updates=True)
